57f75bf9f9a4903d1aba08ca48293533077edb71
RizkixSuputro <buildrizkiporter@gmail.com>
2026-04-11 04:34:31 +0800

    edit (login.php) : Menghapus for dari smeua tag label

--- auth/login.php
+++ auth/login.php
@@ -18,12 +18,12 @@
                 <!-- form pengisian login -->
                 <form action="process_login.php" method="POST">
                     <div class="mb-3 mt-5">
-                        <label for="exampleInputEmail1" class="form-label">Email address</label>
+                        <label class="form-label">Email address</label>
                         <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" required>
                         <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
                     </div>
                     <div class="mb-3">
-                        <label for="exampleInputPassword1" class="form-label">Password</label>
+                        <label class="form-label">Password</label>
                         <input type="password" name="password" class="form-control" id="exampleInputPassword1" required>
                     </div>
                     <button type="submit" class="btn btn-primary w-100">Login</button>
