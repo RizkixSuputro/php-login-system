29458a050b0c0a54f6b38a4dc70ad5fc7d4c7e60
RizkixSuputro <buildrizkiporter@gmail.com>
2026-04-11 18:57:13 +0800

    feat : Menambahkan fitur login dengan session dan redirect ke halaman landing.php

---
+++ auth/process_login.php
@@ -0,0 +1,46 @@
+<?php
+session_start();
+
+require_once "../config/database.php";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    if (empty($email) || empty($password)) {
+        echo "Email dan Password harus diisi";
+    }
+
+    // membuat query
+    $sql = "SELECT * FROM users WHERE email = ?";
+
+    // prepare statement
+    $stmt = mysqli_prepare($conn, $sql);
+
+    // bind parameter
+    mysqli_stmt_bind_param($stmt, "s", $email);
+
+    // eksekusi query
+    mysqli_stmt_execute($stmt);
+
+    // ambil hasil query
+    $result = mysqli_stmt_get_result($stmt);
+
+    // cek apakah ada user
+    if (mysqli_num_rows($result) === 1) {
+        $user = mysqli_fetch_assoc(($result));
+        echo "Email terdaftar";
+    } else {
+        echo "Email belum terdaftar";
+    }
+
+    if (password_verify($password, $user['password'])) {
+        $_SESSION['username_id'] = $user['id'];
+        $_SESSION['username'] = $user['username'];
+
+        header("Location: ../landing.php");
+        exit;
+    } else {
+        echo "Password salah";
+    }
+}
