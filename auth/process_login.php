dc90aa53dbb6e6b9f5b69c5f1a0a54034538eb1d
RizkixSuputro <buildrizkiporter@gmail.com>
2026-04-19 18:07:14 +0800

    fix: perbaiki redirect login ke halaman utama refactor: ubah landing page menjadi index

--- auth/process_login.php
+++ auth/process_login.php
@@ -27,20 +27,26 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $result = mysqli_stmt_get_result($stmt);
 
     // cek apakah ada user
-    if (mysqli_num_rows($result) === 1) {
-        $user = mysqli_fetch_assoc(($result));
-        echo "Email terdaftar";
-    } else {
-        echo "Email belum terdaftar";
+    if (empty($email) || empty($password)) {
+        echo "Email dan Password harus diisi";
+        exit;
     }
 
-    if (password_verify($password, $user['password'])) {
-        $_SESSION['username_id'] = $user['id'];
-        $_SESSION['username'] = $user['username'];
+    if (mysqli_num_rows($result) === 1) {
 
-        header("Location: ../landing.php");
-        exit;
+        $user = mysqli_fetch_assoc($result);
+
+        if (password_verify($password, $user['password'])) {
+
+            $_SESSION['user_id'] = $user['id'];
+            $_SESSION['username'] = $user['username'];
+
+            header("Location: ../index.php");
+            exit;
+        } else {
+            echo "Password salah";
+        }
     } else {
-        echo "Password salah";
+        echo "Email belum terdaftar";
     }
 }
