c79d9a8cd0c270ea6d024c111586b97a7f58aec8
RizkixSuputro <buildrizkiporter@gmail.com>
2026-04-19 17:29:32 +0800

    feat : Membuat halaman landing

---
+++ landing.php
@@ -0,0 +1,86 @@
+<?php
+session_start();
+
+
+?>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Login Page</title>
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</head>
+
+<body>
+
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Porter Media</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Service</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Contact Us</a>
+                    </li>
+                </ul>
+                <div class="d-flex gap-3 align-items-center">
+                    <h5>Hi, <?php echo $_SESSION['username']; ?></h5>
+                    <a href="./auth/logout.php" class="btn btn-primary">Log Out</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+    <!-- main -->
+    <div class="container py-5">
+        <div class="text-center mb-5">
+            <h1 class="fw-bold">Selamat Datang, <?= $_SESSION['username']; ?></h1>
+            <p class="text-muted">
+                Anda berhasil login ke sistem PHP Authentication.
+            </p>
+        </div>
+
+        <!-- begin : Card section -->
+        <div class="row g-4">
+            <div class="card shadow-sm h-100">
+                <div class="card-body">
+                    <h5>Dashboard</h5>
+                    <p>Kelola data dengan mudah.</p>
+                </div>
+            </div>
+            <div class="card shadow-sm h-100">
+                <div class="card-body">
+                    <h5>Dashboard</h5>
+                    <p>Kelola data dengan mudah.</p>
+                </div>
+            </div>
+            <div class="card shadow-sm h-100">
+                <div class="card-body">
+                    <h5>Dashboard</h5>
+                    <p>Kelola data dengan mudah.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <footer class="text-center bg-black py-4 text-white">
+        © 2026 Porter Media
+    </footer>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+</body>
+
+</html>
